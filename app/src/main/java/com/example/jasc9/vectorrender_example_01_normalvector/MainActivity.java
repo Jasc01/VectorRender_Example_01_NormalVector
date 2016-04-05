beb6c508782c07b14ce8c8e5200b5cec294bceab
jasc01 <jasc996@gmail.com>
2016-04-05 04:21:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/jasc9/vectorrender_example_01_normalvector/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jasc9.vectorrender_example_01_normalvector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
